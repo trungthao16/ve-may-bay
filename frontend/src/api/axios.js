15f7081c1b41b17539d7c201f11fd6b0506c0aeb
trungthao19 <trungthao104@gmail.com>
2026-03-25 11:04:14 +0700

    Prepare frontend for vercel deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,18 +1,35 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers = config.headers || {};
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-API.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
-    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
-export default API;
+export default api;
