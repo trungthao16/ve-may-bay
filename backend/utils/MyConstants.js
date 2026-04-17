c7abc89313ce9672c2290e2fe5516504ba117db7
trungthao19 <trungthao104@gmail.com>
2026-04-17 20:51:10 +0700

    Cập nhật Database mới và chuẩn bị đẩy lên Repository mới

--- backend/utils/MyConstants.js
+++ backend/utils/MyConstants.js
@@ -2,7 +2,7 @@ const MyConstants = {
   DB_SERVER: 'cluster0.pqngx0j.mongodb.net',
   DB_USER: 'backenduser',
   DB_PASS: 'Backend2025',
-  DB_DATABASE: 'train_ticket',
+  DB_DATABASE: 'vemaybay',
 
   JWT_SECRET: 'train_ticket_secret',
   JWT_EXPIRES: 24 * 60 * 60
